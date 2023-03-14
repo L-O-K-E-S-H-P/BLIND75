dbf2d4d33b3d6e7fe5822bb34eda4eb7b7d31ce6
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-03-14 07:51:26 +0530

    Create 18-Rotate String.py

---
+++ Python/18-Rotate String.py
@@ -0,0 +1,7 @@
+'''Input: s = "abcde", goal = "cdeab"
+Output: true'''
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s)==len(goal) and  goal in s+s
+  
